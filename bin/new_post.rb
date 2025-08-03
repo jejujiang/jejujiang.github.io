5507b0b17b9e96efc2e147641a101b81204f5e36
jeju <jejujiang@foxmail.com>
2025-08-03 15:46:50 +0800

    initialize

--- bin/new_post.rb
+++ bin/new_post.rb
@@ -1,17 +1,59 @@
 #!/usr/bin/env ruby
-
-# forzen_string_literal: true
+# frozen_string_literal: true
 
 require 'date'
+require 'fileutils'
 
-POSTS_DIR = '_posts'
+POSTS_DIR = '_posts'.freeze
 
-post = ARGV[0]
+def main
+  if ARGV.empty?
+    puts "Usage: #{$PROGRAM_NAME} <post_title> [--open]"
+    exit(1)
+  end
 
-File.open("#{POSTS_DIR}/#{Date.today.strftime('%Y-%m-%d-')}#{post}.md", "w") do |f|
-    f.write(
-        "---\ntitle: \n---"
-    )
+  post_title = ARGV[0]
+  open_after_creation = ARGV[1] == '--open'
+
+  #Create posts directory if it doesn't exist
+  FileUtils.mkdir_p(POSTS_DIR)
+
+  filename = "#{POSTS_DIR}/#{generate_filename(post_title)}.md"
+
+  if File.exist?(filename)
+    puts "[!] Error: File #{filename} already exists!"
+    exit(1)
+  end
+
+  create_post_file(filename, post_title)
+  puts "[+] Created post: #{filename}"
+
+  if open_after_creation
+    system("#{ENV['EDITOR'] || 'vi' } #{filename}")
+  end
 end
 
-puts("[+] Created #{post} post")
+def generate_filename(title)
+  sanitized_title =title.downcase.gsub(/[^a-z0-9\s]/, '').gsub(/\s+/, '-')
+  "#{Date.today.iso8601}-#{sanitized_title}"
+end
+
+def create_post_file(filename, title)
+  File.open(filename, 'w') do |f|
+    f.write(<<~YAML
+            ---
+            title: #{title}
+            date: #{DateTime.now.iso8601}
+            categories: []
+            tags: []
+            ---
+
+            ## Introduction
+
+            Your content here...
+            YAML
+           )
+  end
+end
+
+main if __FILE__ == $PROGRAM_NAME
